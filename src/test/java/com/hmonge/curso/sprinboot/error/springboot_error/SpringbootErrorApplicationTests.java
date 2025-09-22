0a38427ad9ad9dc115bf86d697802e07a655de01
Hernando Monge <hemogem@gmail.com>
2025-09-21 21:36:16 -0600

    first commit

---
+++ src/test/java/com/hmonge/curso/sprinboot/error/springboot_error/SpringbootErrorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hmonge.curso.sprinboot.error.springboot_error;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootErrorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
